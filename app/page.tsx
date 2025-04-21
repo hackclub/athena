cb26e179e3d7e807ee305d4f45ebe73d4c68d897
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2025-04-21 17:50:56 -0500

    Re-formatted Athena Awards banner + added stories page card

--- app/page.tsx
+++ app/page.tsx
@@ -21,19 +21,33 @@ export default async function Home() {
         <div className="text-xl md:text-3xl font-bold mt-2">is a group of programs at Hack Club to empower girls and nonbinary teenagers to code.</div>
         <div className="text-base md:text-lg">From hosting in-person hackathons to virtual workshops, Hack Club is a place to become more technical and immerse yourself in coding.</div>
         
-        <div className='w-full h-fit grid lg:grid-rows-1 grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-8 my-8'>
-         
-          <div className="col-span-full md:col-span-full w-full h-[70vh] relative rounded-lg pb-8 p-9 bg-gradient-to-l from-[#692229] via-[#8C2E37] to-[#993E47] overflow-hidden transition">
-            <div className="relative z-0 h-full">
-              <div className="text-lg md:text-xl font-bold text-white mb-3">Presenting the Athena Awards:</div>
-              <Image alt="Athena Awards Event" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/bdc8c09039207e203df13effe406e2d289e24a13_image.png" className="md:max-h-[40vh] object-cover w-auto" width={1121} height={390} />
-              <div className="text-white text-xl md:w-3/5 line-clamp-4 py-4">Earn a <b>funded trip to a hackathon in New York City</b> as well as other <b>awesome prizes</b> by spending time coding during the <b>Athena Awards</b>, an event for gender minorities in STEM.</div>
-              <Link href="/awards" className="absolute bottom-0 border border-white/30 bg-[#F34B5C]/60 rounded-lg p-4 uppercase text-white inline-block text-lg hover:-rotate-[4deg] hover:p-5 font-bold decoration-transparent transition-all hover:decoration-white">Sign up for free stickers</Link>
+        <div className='w-full h-fit grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-8 my-8'>
+          
+          <div className="h-fit flex flex-col col-span-full lg:col-span-3 grid grid-cols-1 lg:grid-cols-3 gap-8 items-center justify-center">
+            {/* Athena Awards */}
+            <div className="h-full lg:h-[340px] col-span-full lg:col-span-2 flex flex-col lg:flex-row justify-between h-full w-full relative rounded-lg pb-8 p-9 bg-gradient-to-l from-[#692229] via-[#8C2E37] to-[#993E47] overflow-hidden transition">
+              <div className="relative z-0 h-full w-full lg:w-1/2">
+                <div className="text-lg md:text-xl font-bold text-white mb-3">Presenting the Athena Awards:</div>
+                <div className="text-white text-lg xl:text-xl md:w-full py-4">
+                  Earn a <b>funded trip to a hackathon in New York City</b> as well as other <b>awesome prizes</b> by spending time coding during the <b>Athena Awards</b>, an event for gender minorities in STEM.
+                </div>
+                <Link href="/awards" className="block lg:hidden xl:block w-fit border border-white/30 bg-[#F34B5C]/60 rounded-lg p-4 uppercase text-white inline-block text-lg hover:-rotate-[4deg] hover:p-5 font-bold decoration-transparent transition-all hover:decoration-white whitespace-nowrap">
+                  Sign up for free stickers
+                </Link>
+              </div>
+              <div className="z-9999 h-full w-full lg:w-1/2 flex flex-col gap-5 items-start lg:mt-2 lg:items-center justify-center">
+                <img alt="Athena Awards Event" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/bdc8c09039207e203df13effe406e2d289e24a13_image.png" className="md:max-h-[250px] w-auto" />
+                <Link href="/awards" className="hidden lg:block xl:hidden border border-white/30 bg-[#F34B5C]/60 rounded-lg p-4 uppercase text-white inline-block text-normal xl:text-lg hover:-rotate-[4deg] hover:p-5 font-bold decoration-transparent transition-all hover:decoration-white whitespace-nowrap">
+                  Sign up for free stickers
+                </Link>
+              </div>
             </div>
-            <Image alt="" src="https://hc-cdn.hel1.your-objectstorage.com/s/v3/a7004ed60a6669e4fda5afab93d6894b4dc03e21_demo_logo.svg" className="h-[100%] w-auto absolute -top-[4vh] -right-[10vh] opacity-25 md:opacity-100" height={800} width={800}></Image>
+            {/* Stories */}
+            <Card cardType="tinted" image="http://localhost:3002/_next/image?url=https%3A%2F%2Fcloud-2i9tgp21k-hack-club-bot.vercel.app%2F0p1050166.jpg&w=1920&q=75" tintColor="#69346D" href="/stories" title="Stories" description="Check out the awesome projects and stories from Athena members!" className="lg:col-span-1 h-[120px] h-[340px] lg:max-h-[500px] self-stretch" />
           </div>
 
-          <div className="col-span-full md:col-span-full w-full h-full relative rounded-lg pb-8 p-9 bg-gradient-to-l from-[#150122] via-[#150122] to-[#2651A6] overflow-hidden transition">
+          {/* Ascend */}
+          <div className="hidden col-span-full w-full h-[325px] relative rounded-lg pb-8 p-9 bg-gradient-to-l from-[#150122] via-[#150122] to-[#2651A6] overflow-hidden transition">
             <div className="relative z-0">
               <div className="text-lg md:text-xl font-bold text-white mb-3">Our 2024 summit:</div>
               <Image alt="Ascend Event" src="https://cloud-rnls34zc7-hack-club-bot.vercel.app/0reduced-whitespace-ascend-logo.png" className="max-h-[15vh] w-auto" width={1121} height={390} />
@@ -43,19 +57,7 @@ export default async function Home() {
             <Image alt="" src="https://ascend.hackclub.com/moon.png" className="h-[150%] w-auto absolute -top-[10vh] -right-[10vh] opacity-50 md:opacity-100" height={800} width={800}></Image>
           </div>
         </div>
-        <Link href='/stories' className='hidden w-full h-full col-span-full md:col-span-2'>
-            <div className="w-full h-full relative flex flex-col gap-3 bg-white shadow-md rounded-lg transition hover:scale-110 cursor-pointer">
-              <Image alt="Ascend Event" src="https://cloud-l9vmg3n5i-hack-club-bot.vercel.app/0img_5774.jpg" className="h-[120px] w-full object-cover rounded-t-lg" width={300} height={163} />
-              <div className="relative z-0 pb-4 px-4">
-                <h3 className="text-lg md:text-2xl font-bold text-red">Stories Showcase 🎉</h3>
-                <div className="text-sm md:text-base">from #athena-team</div>
-                <br/>
-                <div className="text-black md:w-full">
-                Check out the awesome projects and stories from Athena members! Dive in to see how creativity and teamwork shine in our community.  
-                </div>
-              </div>
-            </div> 
-          </Link>
+
         {/* TODO: set grid-rows on large back to 2 */}
         <div id="bento" className="w-full grid lg:grid-rows-1 grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-8 my-8">
           <Card cardType="tinted" image="https://cloud-lz7wipd7a-hack-club-bot.vercel.app/1events-card-min.jpg" tintColor="#69346D" href="/events" title="Events" description="One-day coding events that help girls and gender minorities write their first lines of code" className="md:col-span-1 lg:col-span-2 self-stretch" />
