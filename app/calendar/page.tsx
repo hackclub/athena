000f14e865cafc910c708e5d2d40b94e696a8d50
phoebe <phoebe.contactme@protonmail.com>
2025-06-12 12:51:02 -0400

    Improving: loading styling

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -5,6 +5,7 @@ import UpcomingCalendarSection from "@/components/calendar/UpcomingCalendarSecti
 import Link from "next/link";
 import { FaArrowLeftLong } from "react-icons/fa6";
 import { useState, useEffect } from "react";
+import Background from "@/components/Background";
 
 export default function Calendar() {
   const [currentDate, setCurrentDate] = useState<Date | null>(null);
@@ -14,7 +15,15 @@ export default function Calendar() {
   }, []);
 
   if (!currentDate) {
-    return <div>Loading...</div>;
+    return (
+      <Background>
+        <div className="min-h-screen flex flex-col items-center justify-center">
+          <div className="space-y-2">
+              <h2 className="text-3xl font-bold text-[#D35648]">loading calendar...</h2>
+            </div>
+        </div>
+      </Background>
+    )
   }
 
   const upcomingEvents = CALENDAR_EVENTS.filter(e => e.date > currentDate);
