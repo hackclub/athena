8cadb13999eace7ea34b2856ddb11c92e2693236
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2025-06-25 22:15:18 -0500

    Changed base project image

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,2 @@
-export const baseAthenaAwardProjectImageUrl = 'https://hc-cdn.hel1.your-objectstorage.com/s/v3/d188e67b796c30a7efd3f392c4533b1566db4dcc_image.png'
+export const baseAthenaAwardProjectImageUrl =
+  "https://hc-cdn.hel1.your-objectstorage.com/s/v3/c1fa04d761598c7039f10a5aad93138a9e83f84c_a86775ba70e6f71f9aa8e9619a4c7f89d17a6f6d_image__fliter--sepia__channel--red__mode--normal_1.png";
