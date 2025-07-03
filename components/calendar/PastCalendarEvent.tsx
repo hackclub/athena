2befc4f8e208c5bfeccadc47a56cc086b1132bd1
aqua <84078890+phthallo@users.noreply.github.com>
2025-07-03 12:39:12 -0400

    weird misalignment

--- components/calendar/PastCalendarEvent.tsx
+++ components/calendar/PastCalendarEvent.tsx
@@ -54,7 +54,7 @@ export default function PastCalEvent({ name, date, description, hostedBy, record
                     </div>
 
                     {recording && <a href = {recording}><span className="uppercase inline-block bg-green-600/10 text-green-600 text-xs px-2 py-1 rounded-full mt-2 mr-2 *:align-middle">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 inline pr-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 inline pr-1">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
                         </svg>
                         <span>video</span>
