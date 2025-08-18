393a15816cb7aea1506ab9b1097de08d8f0e4464
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2025-08-18 03:10:02 -0500

    Update stories page

--- lib/stories.ts
+++ lib/stories.ts
@@ -3,70 +3,133 @@ export const stories = [
     title: "CourtConnect",
     author: "Celeste R.",
     date: "2024",
-    imageUrl: "https://hc-cdn.hel1.your-objectstorage.com/s/v3/19a19adc4258c91890e0cd382d2bec001b633286_image.png",
+    imageUrl:
+      "https://hc-cdn.hel1.your-objectstorage.com/s/v3/19a19adc4258c91890e0cd382d2bec001b633286_image.png",
     featured: true,
     award: true,
-    description: "Winning app of the 2024 Congressional App Challenge in Georgia's Fifth District! CourtConnect is an app designed to bridge communication gaps between judges and jurors, making jury duty more efficient, accessible, and less frustrating. Inspired by her mother, a Superior Court Judge, Celeste built this to help improve the court experience while ensuring time and resources aren't wasted.",
-    demoLink: "https://www.congressionalappchallenge.us/24-ga05/"
+    description:
+      "Winning app of the 2024 Congressional App Challenge in Georgia's Fifth District! CourtConnect is an app designed to bridge communication gaps between judges and jurors, making jury duty more efficient, accessible, and less frustrating. Inspired by her mother, a Superior Court Judge, Celeste built this to help improve the court experience while ensuring time and resources aren't wasted.",
+    demoLink: "https://www.congressionalappchallenge.us/24-ga05/",
   },
   {
     title: "NigiriPad",
     author: "Jeslyn C.",
     date: "2024",
-    imageUrl: "https://hc-cdn.hel1.your-objectstorage.com/s/v3/48c4b4cd4f7a6ba52429f5baa2b2dbc01ffa87e0_image.png",
+    imageUrl:
+      "https://hc-cdn.hel1.your-objectstorage.com/s/v3/48c4b4cd4f7a6ba52429f5baa2b2dbc01ffa87e0_image.png",
     featured: true,
     award: false,
-    description: "A beautifully designed 3-key macropad inspired by sushi! Jeslyn's NigiriPad combines hardware, design, and functional aesthetics to create a custom macropad with hand-painted details.",
-    demoLink: "https://github.com/se1yu/NigiriPad"
+    description:
+      "A beautifully designed 3-key macropad inspired by sushi! Jeslyn's NigiriPad combines hardware, design, and functional aesthetics to create a custom macropad with hand-painted details.",
+    demoLink: "https://github.com/se1yu/NigiriPad",
   },
   {
     title: "Skygazer",
     author: "Phaedra S. & Zoya H.",
     date: "2024",
-    imageUrl: "https://hc-cdn.hel1.your-objectstorage.com/s/v3/9885407fc2b748c0aad309b0fbf5b50552647ecd_image.png",
+    imageUrl:
+      "https://hc-cdn.hel1.your-objectstorage.com/s/v3/9885407fc2b748c0aad309b0fbf5b50552647ecd_image.png",
     featured: true,
     award: true,
-    description: "Skygazer is a tool that helps you find the best stargazing spots near you! Just type in an address, and Skygazer finds an optimal location for clear skies, avoiding light pollution hotspots. This awesome project was built at Ascend, our space-themed hackathon for gender minorities in tech!",
-    demoLink: "https://github.com/ph4iry/skygazer"
+    description:
+      "Skygazer is a tool that helps you find the best stargazing spots near you! Just type in an address, and Skygazer finds an optimal location for clear skies, avoiding light pollution hotspots. This awesome project was built at Ascend, our space-themed hackathon for gender minorities in tech!",
+    demoLink: "https://github.com/ph4iry/skygazer",
   },
   {
     title: "F1 Car Aero Design",
     author: "Meghana M.",
     date: "2024",
-    imageUrl: "https://hc-cdn.hel1.your-objectstorage.com/s/v3/0797e2c84353bdc291c900f88591855ef1465acc_image.png",
+    imageUrl:
+      "https://hc-cdn.hel1.your-objectstorage.com/s/v3/0797e2c84353bdc291c900f88591855ef1465acc_image.png",
     featured: true,
     award: false,
-    description: "Meghana designed the aerodynamic structure of an F1 car and is getting it resin printed soon! This unique project shows us a beautiful blend of cutting-edge design and hands-on engineering.",
-    demoLink: "https://github.com/MeghanaM4/MAD16"
+    description:
+      "Meghana designed the aerodynamic structure of an F1 car and is getting it resin printed soon! This unique project shows us a beautiful blend of cutting-edge design and hands-on engineering.",
+    demoLink: "https://github.com/MeghanaM4/MAD16",
   },
   {
     title: "Tic-Tac-Toe AI",
     author: "Paperclip",
     date: "2024",
-    imageUrl: "https://hc-cdn.hel1.your-objectstorage.com/s/v3/98e3d75246b9c313bc7561df5f4f67729c8335a1_image.png",
+    imageUrl:
+      "https://hc-cdn.hel1.your-objectstorage.com/s/v3/98e3d75246b9c313bc7561df5f4f67729c8335a1_image.png",
     featured: false,
     award: false,
-    description: "A fun and interactive classic remake, and also their very first project in JS and TS! The Tic-Tac-Toe AI uses the Minimax algorithm to make the game way more exciting by creating an intelligent opponent that plays optimally. This project is a great first step in exploring the world of AI and trying new coding techniques!",
-    demoLink: "https://github.com/magical-paperclip/tic-tac-toe-ai"
+    description:
+      "A fun and interactive classic remake, and also their very first project in JS and TS! The Tic-Tac-Toe AI uses the Minimax algorithm to make the game way more exciting by creating an intelligent opponent that plays optimally. This project is a great first step in exploring the world of AI and trying new coding techniques!",
+    demoLink: "https://github.com/magical-paperclip/tic-tac-toe-ai",
   },
   {
     title: "POKER? I HARDLY KNOW HER!",
     author: "Sahiti",
     date: "2024",
-    imageUrl: "https://hc-cdn.hel1.your-objectstorage.com/s/v3/a6ba76bddfbcaaff00d87e26ad20a5ff57c8b228_image.png",
+    imageUrl:
+      "https://hc-cdn.hel1.your-objectstorage.com/s/v3/a6ba76bddfbcaaff00d87e26ad20a5ff57c8b228_image.png",
     featured: true,
     award: true,
-    description: "What started as a random airport encounter turned into a hilarious Scrapyard project using Ray-Ban Meta AI glasses. Sahiti, Arnav, and Tejas created a system that lets you play poker and do homework at the same time – but the better you do at one, the worse you perform at the other. These scrappy glasses analyze your poker hand, the table, and the players' emotions to predict your chances, then help with your homework, but with a twist. Using OpenCV, OpenAI, and Python, they made it happen!",
-    demoLink: "https://youtu.be/JVtFxCJw5ng"
+    description:
+      "What started as a random airport encounter turned into a hilarious Scrapyard project using Ray-Ban Meta AI glasses. Sahiti, Arnav, and Tejas created a system that lets you play poker and do homework at the same time – but the better you do at one, the worse you perform at the other. These scrappy glasses analyze your poker hand, the table, and the players' emotions to predict your chances, then help with your homework, but with a twist. Using OpenCV, OpenAI, and Python, they made it happen!",
+    demoLink: "https://youtu.be/JVtFxCJw5ng",
   },
   {
     title: "Trash Talking Trash Can",
     author: "Meghana",
     date: "2024",
-    imageUrl: "https://hc-cdn.hel1.your-objectstorage.com/s/v3/8040331ac3036f8623cf3ec287b3ac81fda25dc4_image.png",
+    imageUrl:
+      "https://hc-cdn.hel1.your-objectstorage.com/s/v3/8040331ac3036f8623cf3ec287b3ac81fda25dc4_image.png",
     featured: true,
     award: false,
-    description: "Have you ever wondered what your trash can would say if it could talk back to you? Probably not, but that's what makes this next project ridiculously amazing. Say hello to the TTTC! The most scrappy, rude, and downright hilarious trash can ever created. Meghana, Ducc, and Selena worked nonstop through the weekend to bring this trash talker to life. The result? A trash can that doesn't just collect garbage but also throws it right back at ya verbally.",
-    demoLink: "https://youtu.be/JxGyt0A78mU?si=T_X8lFCxaqJ8Zki7"
-  }
-]
+    description:
+      "Have you ever wondered what your trash can would say if it could talk back to you? Probably not, but that's what makes this next project ridiculously amazing. Say hello to the TTTC! The most scrappy, rude, and downright hilarious trash can ever created. Meghana, Ducc, and Selena worked nonstop through the weekend to bring this trash talker to life. The result? A trash can that doesn't just collect garbage but also throws it right back at ya verbally.",
+    demoLink: "https://youtu.be/JxGyt0A78mU?si=T_X8lFCxaqJ8Zki7",
+  },
+  {
+    title: "Volume Breakout",
+    author: "Rosewater (@r0s3)",
+    date: "2025",
+    imageUrl:
+      "https://hc-cdn.hel1.your-objectstorage.com/s/v3/8040331ac3036f8623cf3ec287b3ac81fda25dc4_image.png",
+    featured: true,
+    award: false,
+    description:
+      "It’s Breakout, but with a twist, you control the paddle using the 3DS volume slider. Yep, she built a fully playable clone for the 3DS that turns audio control into game control, and it’s just as chaotic and fun as it sounds. Made with Citro2D, libctru, and devkitARM, this was their first 3DS project. The idea came from the simple fact that no one had done it yet, so they did. P.S. Acon even got it running on their own 3DS!",
+    demoLink:
+      "https://hc-cdn.hel1.your-objectstorage.com/s/v3/10dfa7b1b7788a81510752199f451668201d521c_image.png",
+  },
+  {
+    title: "Subject-13",
+    author: "Jessica Sun (@Jessica Sun)",
+    date: "2025",
+    imageUrl:
+      "https://hc-cdn.hel1.your-objectstorage.com/s/v3/44bc46ee0ae6680b79b51c01e3cd6df4b0c37c0c_image.png",
+    featured: true,
+    award: false,
+    description:
+      "Step into a mysterious underground lab in this choose-your-own-adventure game where your choices decide whether you escape or disappear. Jessica built Subject-13 so you can play as either a scientist or a test subject, each with totally different storylines, decisions, and even a secret ending (if you’re sharp enough to find it).",
+    demoLink: "https://jessicasun1022.github.io/Subject-13/",
+  },
+  {
+    title: "Infinity Lost",
+    author: "Violet (@ViviTheQueen)",
+    date: "2025",
+    imageUrl:
+      "https://hc-cdn.hel1.your-objectstorage.com/s/v3/1946eff457cc5ef55c25005ba44fefcfb8fb6450_image.png",
+    featured: true,
+    award: false,
+    description:
+      "The game where every exit just leads you back to the start: welcome to the infinitely looping space shuttle. As John Loop, you’ll wall-run, dash, and battle through twisting corridors in this high-speed movement FPS. Violet built Infinity Lost by combining the Godot game engine with TrenchBroom-designed levels to deliver fluid combat, tight movement, and non-stop momentum!",
+    demoLink: "https://vivithequeen.itch.io/infinity-lost",
+  },
+  {
+    title: "Kamochi",
+    author: "Ren (@ren)",
+    date: "2025",
+    imageUrl:
+      "https://hc-cdn.hel1.your-objectstorage.com/s/v3/bb5f464c1147281d2314f1729ba17c8e8c64d991_image.png",
+    featured: true,
+    award: false,
+    description:
+      "Meet Kamochi, your very own virtual duck! Inspired by Tamagotchi, Ren made this p5.js game where you raise a duck from egg to full-grown, keeping its food, happiness, and sleep stats up by playing adorable minigames. Let any stat hit zero, and it’s game over. All the pixel art (yes, including the cutest duck sprites ever!) was made by Ren in Piskel, and this was also their very first JavaScript project.",
+    demoLink: "https://electricxangel.github.io/kamochi/",
+  },
+];
