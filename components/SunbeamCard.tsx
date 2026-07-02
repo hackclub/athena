310e1d63a500ace36ed5c6236cf3affd8a2713b7
SafiaEzzahir <safia.ezzahir@hotmail.com>
2026-07-01 22:27:04 -0400

    Fix hero section

--- components/SunbeamCard.tsx
+++ components/SunbeamCard.tsx
@@ -59,60 +59,67 @@ export default function SunbeamCard() {
         <div className="absolute bottom-0 left-0 right-0 h-96 bg-gradient-to-b from-transparent to-white pointer-events-none z-10" />
         
         <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "end", fontSize: "1.5vw", position: "relative", bottom: "4vw"}}>
-          <div className="relative z-20" style={{width: "40vw", height: "auto", display: "flex", flexDirection: "column", marginLeft: "0", padding: "0"}}>
+          <div className="relative flex flex-col md:flex-row md:justify-between md:items-center gap-8 md:gap-[4vw]">
 
-            <img
-              src="images/sunbeamlogo.png"
-              alt="Sunbeam"
-              style={{width: "32vw", height: "auto", marginLeft: "1vw"}}
-            />
-
-            <div
-              className="relative w-[95vw] md:w-[38vw] my-[3vh] md:my-[4vh]"
-              style={{ transform: "rotate(-1.57deg)" }}
-            >
-              <Image
-                src="/images/towel.png"
-                width={1434}
-                height={1172}
-                alt=""
-                className="w-full h-auto rounded-2xl"
-                sizes="(max-width: 768px) 95vw, 38vw"
+            {/* left column */}
+            <div className="relative z-20 flex flex-col items-center w-full md:w-[42vw]">
+              <img
+                src="/images/sunbeamlogo.png"
+                alt="Sunbeam"
+                className="w-[70vw] md:w-[30vw] h-auto mt-[3vw]"
               />
+
               <div
-                className="absolute rounded-[32px] backdrop-blur-md bg-[#F393B4]/1 flex items-center justify-center text-center p-[4%]"
-                style={{
-                  top: "18.7%",
-                  left: "14%",
-                  right: "13.3%",
-                  bottom: "12.2%"
-                }}
+                className="relative w-[88vw] md:w-[38vw] mt-[5vw] md:mt-[2.5vw]"
+                style={{ transform: "rotate(-1.57deg)" }}
               >
-                <p style={{fontSize: "2vw", fontWeight: 300, color: "#2E599C"}}>
-                  20+ beginner friendly <br /> social coding events for <br /> girls
-                  around the world <br /> 29th August | sign up to run your own :)
-                </p>
+                <Image
+                  src="/images/towel.png"
+                  width={1434}
+                  height={1172}
+                  alt=""
+                  className="w-full h-auto rounded-2xl"
+                  sizes="(max-width: 768px) 88vw, 38vw"
+                />
+                <div
+                  className="absolute backdrop-blur-sm bg-[#F393B4]/1 flex items-center justify-center text-center rounded-[32px]"
+                  style={{ top: "18.7%", left: "7%", right: "6%", bottom: "12.2%" }}
+                >
+                  <p
+                    className="text-[4vw] md:text-[1.7vw]"
+                    style={{ fontWeight: 400, color: "#2E599C", lineHeight: 1.6 }}
+                  >
+                    20+ <u>beginner friendly</u> <br /> social coding events for <br /> girls
+                    around the world <br /> <span style={{ fontWeight: 600 }}>29th August</span> | sign up to run your own :)
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="relative" style={{width: "50vw", height: "auto", display: "flex", flexDirection: "column", justifyContent: "flex-end"}}>
-            <iframe
-              style={{marginBottom: "2vw"}}
-              className="w-full rounded-lg border-2 border-[#2E599C] mt-4 md:mt-0 lg:mt-8 animate-wiggle hover:animate-none h-[200px] md:h-[300px] lg:h-[415px]"
-              src="https://www.youtube.com/embed/FIyDH5dm5eo"
-              title="This is the Athena Award"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
-            <a href="google.com" style={{position: "relative", width: "25vw", display: "block", alignSelf: "center"}}>
-              <img src="/images/surfboard.png" alt="" style={{width: "25vw", height: "auto", display: "block"}} />
-              <div style={{position: "absolute", inset: 0, display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", color: "#0E387A", fontWeight: 600, fontSize: "1.9vw", marginLeft: "0.2vw"}}>
-                <span>run your own Sunbeam</span>
-              </div>
-            </a>
+            {/* right column */}
+            <div className="relative flex flex-col items-center gap-8 md:gap-[2.5vw] w-full md:w-[46vw]">
+              <iframe
+                className="w-full rounded-lg border-2 border-[#2E599C] animate-wiggle hover:animate-none h-[52vw] md:h-[26vw]"
+                src="https://www.youtube.com/embed/FIyDH5dm5eo"
+                title="This is the Athena Award"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+              <a
+                href="https://sunbeam.hackclub.com/"
+                className="relative block w-[55vw] md:w-[22vw] transition-transform hover:scale-105"
+              >
+                <img src="/images/surfboard.png" alt="" className="w-full h-auto block" />
+                <div
+                  className="absolute inset-0 flex items-center justify-center text-center text-[4vw] md:text-[1.6vw]"
+                  style={{ color: "#0E387A", fontWeight: 600, textShadow: "-2px -2px 0 white, 2px -2px 0 white, -2px 2px 0 white, 2px 2px 0 white" }}
+                >
+                  <span>run your own Sunbeam!</span>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
